6fcd82b6856c077d81330346c86087efc95ba922
tomoyan2222 <assatomo2222@gmail.com>
2020-05-29 18:13:30 +0900

    create views and controller '/users'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
   def top
   end
+
+  def search
+  end
+
+  def sign_up
+  end
+
+  def sign_in
+  end
+
+  def profile
+  end
+
+  def follow
+  end
+
+  def follower
+  end
+
+  def word
+  end
+
+  def likes
+  end
+
+  def edit
+  end
+
+  def important
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to:'users#top'
+  get '/sign_in', to:'users#sign_in'
+  get '/sign_up', to:'users#sign_up'
+  get '/users/search', to:'users#search'
+  get '/users/profile/:id', to:'users#profile'
+  get '/users/follow/:id', to:'users#follow'
+  get '/users/follower/:id', to:'users#follower'
+  get '/users/word', to:'users#word'
+  get '/users/likes', to:'users#likes'
+  get '/users/edit/:id', to:'users#edit'
+  get '/users/important/:id', to:'users#important'
 end
