d50da69b9499aa65c5d1f8f6704b4bda11cc2c4f
tomoyan2222 <assatomo2222@gmail.com>
2020-06-03 12:30:19 +0900

    g model users

---
+++ db/migrate/20200603032842_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :original_name
+      t.string :password_digest
+      t.string :image
+      t.string :profile
+
+      t.timestamps
+    end
+  end
+end
