db44dc4cbf51a5fceb2161e973d677fa74b51340
tomoyan2222 <assatomo2222@gmail.com>
2020-06-05 19:01:17 +0900

    add dependent: :destroy

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,7 +1,7 @@
 class Title < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_many :vocabularies
+  has_many :vocabularies, dependent: :destroy
 
   validates :name, presence: true
 end
