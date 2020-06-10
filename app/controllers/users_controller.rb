a3b7dabe9512cf8051964532736f196bc5fd35b2
tomoyan2222 <assatomo2222@gmail.com>
2020-06-10 12:42:16 +0900

    change user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,7 @@ class UsersController < ApplicationController
 
   def word
     @user = User.find(params[:id])
-    @title = Title.where(user_id: current_user.id)
+    @title = Title.where(user_id: @user.id)
   end
 
   def likes
