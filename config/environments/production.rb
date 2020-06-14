0c3df0ae2128d820631efbe0e61bf931ab3a8bec
tomoyan2222 <assatomo2222@gmail.com>
2020-06-14 14:09:15 +0900

    adjust for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
-  config.assets.compile = true
-  config.assets.initialize_on_precompile=false
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -29,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
