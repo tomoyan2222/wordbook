643ec9e6e10281aa246d84a751c87c1e8a8b532d
tomoyan2222 <assatomo2222@gmail.com>
2020-06-15 15:29:30 +0900

    change regular expressions

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -1,7 +1,7 @@
 class Vocabulary < ApplicationRecord
   belongs_to :title
 
-  WORD_FORMAT = /\A[a-zA-Z0-9]+\z/
+  WORD_FORMAT = /\A[a-zA-Z0-9]/
 
   validates :word, presence: true, format: {with: WORD_FORMAT}, length: { maximum: 74 }
   validates :meaning, presence: true
